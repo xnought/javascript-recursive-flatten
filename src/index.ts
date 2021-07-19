ad84969662b4ef649b5cdcbaf730472faef1fcdf
xnought <bertuccd@oregonstate.edu>
2021-07-19 13:15:19 -0700

    add prettier settings for formatting

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
 import recursiveFlatten from "./recursiveFlatten";
 
 function main() {
-	const data = [1, [2, 3, [4, 5], 6], 7, [8, 9, [[10]]]];
-	const flattened = recursiveFlatten(data);
-	console.log(`flattened = [${flattened}]`);
-	// output>>> flattened = [1,2,3,4,5,6,7,8,9,10]
+    const data = [1, [2, 3, [4, 5], 6], 7, [8, 9, [[10]]]];
+    const flattened = recursiveFlatten(data);
+    console.log(`flattened = [${flattened}]`);
+    // output>>> flattened = [1,2,3,4,5,6,7,8,9,10]
 }
 function crazy() {
-	// prettier-ignore
-	const data = [[[[[[[[[[[[[[[[[[[[[[[[[[[1], [[[[[[[[[[[[2]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]], [[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[3]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]];
-	const flattened = recursiveFlatten(data);
-	console.log(`flattened = [${flattened}]`);
-	// output>>> flattened = [1,2,3,4,5,6,7,8,9,10]
+    // prettier-ignore
+    const data = [[[[[[[[[[[[[[[[[[[[[[[[[[[1], [[[[[[[[[[[[2]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]], [[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[3]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]];
+    const flattened = recursiveFlatten(data);
+    console.log(`flattened = [${flattened}]`);
+    // output>>> flattened = [1,2,3,4,5,6,7,8,9,10]
 }
 
 crazy();
